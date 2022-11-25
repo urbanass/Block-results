503191c4192b5d042df9e42ac6286bdf21dd9157
UrbanAss134 <urbanass@bk.ru>
2022-11-25 17:37:54 +0500

    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[6] { "hello", "777", "33", "paint", "qwerty", "Z" };
+string[] array2 = new string[array1.Length];
+void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+Array(array1, array2);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(array2);
