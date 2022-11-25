9493945c996243568b50179167d2df52b42b7119
UrbanAss134 <urbanass@bk.ru>
2022-11-25 18:24:20 +0500

    добавил диарнамму

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 string[] array2 = new string[array1.Length];
 void Array(string[] array1, string[] array2)
 {
-    int count = 0;
+    int j = 0;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
-            array2[count] = array1[i];
-            count++;
+            array2[j] = array1[i];
+            j++;
         }
     }
 }
